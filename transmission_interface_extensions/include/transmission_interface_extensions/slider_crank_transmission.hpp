d84b13ce3d3e9fa3f3ae3a8ddee2522bf5781b58
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-12-16 22:11:00 +0900

    add slider_crank_transmission

---
+++ transmission_interface_extensions/include/transmission_interface_extensions/slider_crank_transmission.hpp
@@ -0,0 +1,198 @@
+#ifndef TRANSMISSION_INTERFACE_EXTENSIONS_SLIDER_CRANK_TRANSMISSION_HPP
+#define TRANSMISSION_INTERFACE_EXTENSIONS_SLIDER_CRANK_TRANSMISSION_HPP
+
+#include <cmath>
+#include <utility> // for std::size_t
+
+#include <transmission_interface/transmission.h>
+#include <transmission_interface_extensions/common_namespaces.hpp>
+
+namespace transmission_interface_extensions {
+
+struct SliderCrankTransmissionInfo {
+  // actuator params
+  double actuator_reduction;
+
+  // crank joint/link params
+  std::size_t crank_joint_id;
+  double crank_offset;
+  double crank_length;
+
+  // bar joint/link params
+  std::size_t bar_joint_id;
+  double bar_length;
+
+  // slider joint/link params
+  std::size_t slider_joint_id;
+  double slider_offset_x;
+  double slider_offset_y;
+};
+
+class SliderCrankTransmission : public ti::Transmission {
+public:
+  SliderCrankTransmission(const SliderCrankTransmissionInfo &info)
+      : info_(info), present_actuator_pos_(0.), present_slider_pos_(0.) {}
+
+  virtual ~SliderCrankTransmission() {}
+
+  //
+  // required interface as a transmission
+  //
+
+  virtual void actuatorToJointEffort(const ti::ActuatorData &act_data, ti::JointData &jnt_data) {
+    calcJointEffort(*act_data.effort[0], jnt_data.effort[info_.crank_joint_id],
+                    jnt_data.effort[info_.bar_joint_id], jnt_data.effort[info_.slider_joint_id]);
+  }
+
+  virtual void actuatorToJointVelocity(const ti::ActuatorData &act_data, ti::JointData &jnt_data) {
+    calcJointVelocity(*act_data.velocity[0], jnt_data.velocity[info_.crank_joint_id],
+                      jnt_data.velocity[info_.bar_joint_id],
+                      jnt_data.velocity[info_.slider_joint_id]);
+  }
+
+  virtual void actuatorToJointPosition(const ti::ActuatorData &act_data, ti::JointData &jnt_data) {
+    calcJointPosition(*act_data.position[0], jnt_data.position[info_.crank_joint_id],
+                      jnt_data.position[info_.bar_joint_id],
+                      jnt_data.position[info_.slider_joint_id]);
+
+    // assuming this function is called only if converting present actuator to joint positions.
+    // I guess this is only method to get the present actuator/joint positions.
+    present_actuator_pos_ = *act_data.position[0];
+    present_slider_pos_ = *jnt_data.position[info_.slider_joint_id];
+  }
+
+  virtual void jointToActuatorEffort(const ti::JointData &jnt_data, ti::ActuatorData &act_data) {
+    calcActuatorEffort(*jnt_data.effort[info_.slider_joint_id], act_data.effort[0]);
+  }
+
+  virtual void jointToActuatorVelocity(const ti::JointData &jnt_data, ti::ActuatorData &act_data) {
+    calcActuatorVelocity(*jnt_data.velocity[info_.slider_joint_id], act_data.velocity[0]);
+  }
+
+  virtual void jointToActuatorPosition(const ti::JointData &jnt_data, ti::ActuatorData &act_data) {
+    calcActuatorPosition(*jnt_data.position[info_.slider_joint_id], act_data.position[0]);
+  }
+
+  virtual std::size_t numActuators() const { return 1; }
+
+  virtual std::size_t numJoints() const { return 3; }
+
+private:
+  //
+  // actuator -> joint
+  //
+
+  // normalize to (-pi, pi]
+  static double normalizeAngle(double th) {
+    while (th > M_PI) {
+      th -= 2. * M_PI;
+    }
+    while (th <= -M_PI) {
+      th += 2. * M_PI;
+    }
+    return th;
+  }
+
+  void calcJointPosition(const double act_pos, double *crank_jnt_pos, double *bar_jnt_pos,
+                         double *slider_jnt_pos) const {
+    // forward kinematics of slider-crank mechanism in the crank joint frame.
+    const double crank_th(normalizeAngle(act_pos / info_.actuator_reduction + info_.crank_offset));
+    const double bar_x(info_.crank_length * std::cos(crank_th));
+    const double bar_y(info_.crank_length * std::sin(crank_th));
+    const double bar2slider_y(info_.slider_offset_y - bar_y);
+    const double bar2slider_x(
+        std::sqrt(info_.bar_length * info_.bar_length - bar2slider_y * bar2slider_y));
+    const double bar_th(std::atan2(bar2slider_y, bar2slider_x));
+
+    // transform from crank to each joint frames
+    *crank_jnt_pos = crank_th; // already normalized
+    *bar_jnt_pos = normalizeAngle(bar_th - crank_th);
+    *slider_jnt_pos = bar_x + bar2slider_x - info_.slider_offset_x;
+  }
+
+  void calcJointVelocity(const double act_vel, double *crank_jnt_vel, double *bar_jnt_vel,
+                         double *slider_jnt_vel) const {
+    double crank_pos, bar_pos, slider_pos;
+    calcJointPosition(present_actuator_pos_, &crank_pos, &bar_pos, &slider_pos);
+    const double d_act_pos(info_.actuator_reduction * 0.01);
+    double crank_pos2, bar_pos2, slider_pos2;
+    calcJointPosition(present_actuator_pos_ + d_act_pos, &crank_pos, &bar_pos, &slider_pos2);
+
+    // jnt_vel = d(jnt_pos) / dt
+    //         = d(jnt_pos) / d(act_pos) * d(act_pos) / dt
+    //         = d(jnt_pos) / d(act_pos) * act_vel
+    *crank_jnt_vel = normalizeAngle(crank_pos2 - crank_pos) / d_act_pos * act_vel;
+    *bar_jnt_vel = normalizeAngle(bar_pos2 - bar_pos) / d_act_pos * act_vel;
+    *slider_jnt_vel = (slider_pos2 - slider_pos) / d_act_pos * act_vel;
+  }
+
+  void calcJointEffort(const double act_eff, double *crank_jnt_eff, double *bar_jnt_eff,
+                       double *slider_jnt_eff) const {
+    double crank_pos, bar_pos, slider_pos;
+    calcJointPosition(present_actuator_pos_, &crank_pos, &bar_pos, &slider_pos);
+    const double d_act_pos(info_.actuator_reduction * 0.01);
+    double crank_pos2, bar_pos2, slider_pos2;
+    calcJointPosition(present_actuator_pos_ + d_act_pos, &crank_pos2, &bar_pos2, &slider_pos2);
+
+    // From virtual work priciple,
+    //   slider_eff * d(slider_pos) = act_eff * d(act_pos)
+    //   slider_eff = d(act_pos) / d(slider_pos) * act_eff
+    // where assuming crank & bar do no work
+    *crank_jnt_eff = 0.;
+    *bar_jnt_eff = 0.;
+    *slider_jnt_eff = d_act_pos / (slider_pos2 - slider_pos) * act_eff;
+  }
+
+  //
+  // slider joint -> actuator
+  //
+
+  void calcActuatorPosition(const double slider_jnt_pos, double *act_pos) const {
+    // inverse kinematics of slider-crank mechanism in the crank joint frame
+    const double slider_x(slider_jnt_pos + info_.slider_offset_x);
+    const double slider_r(
+        std::sqrt(slider_x * slider_x + info_.slider_offset_y * info_.slider_offset_y));
+    const double crank_th(
+        normalizeAngle(std::atan2(info_.slider_offset_y, slider_x) +
+                       std::acos((slider_r * slider_r + info_.crank_length * info_.crank_length -
+                                  info_.bar_length * info_.bar_length)) /
+                           (2. * slider_r * info_.crank_length)));
+
+    // simple transmission from crank joint to actuator
+    *act_pos = (crank_th - info_.crank_offset) * info_.actuator_reduction;
+  }
+
+  void calcActuatorVelocity(const double slider_jnt_vel, double *act_vel) const {
+    double act_pos;
+    calcActuatorPosition(present_slider_pos_, &act_pos);
+    const double d_slider_pos(0.01);
+    double act_pos2;
+    calcActuatorPosition(present_slider_pos_ + d_slider_pos, &act_pos2);
+
+    // act_vel = d(act_pos) / dt
+    //         = d(act_pos) / d(slider_pos) * d(slider_pos) / dt
+    //         = d(act_pos) / d(slider_pos) * slider_vel
+    *act_vel = (act_pos2 - act_pos) / d_slider_pos * slider_jnt_vel;
+  }
+
+  void calcActuatorEffort(const double slider_jnt_eff, double *act_eff) const {
+    double act_pos;
+    calcActuatorPosition(present_slider_pos_, &act_pos);
+    const double d_slider_pos(0.01);
+    double act_pos2;
+    calcActuatorPosition(present_slider_pos_ + d_slider_pos, &act_pos2);
+
+    // From virtual work priciple,
+    //   act_eff * d(act_pos) = slider_eff * d(slider_pos)
+    //   act_eff = d(slider_pos) / d(act_pos) * slider_eff
+    *act_eff = d_slider_pos / (act_pos2 - act_pos) * slider_jnt_eff;
+  }
+
+private:
+  const SliderCrankTransmissionInfo info_;
+  double present_actuator_pos_, present_slider_pos_;
+};
+
+} // namespace slider_crank_transmission
+
+#endif
---
+++ transmission_interface_extensions/include/transmission_interface_extensions/slider_crank_transmission_loader.hpp
@@ -0,0 +1,114 @@
+#ifndef TRANSMISSION_INTERFACE_EXTENSIONS_SLIDER_CRANK_TRANSMISSION_LOADER_HPP
+#define TRANSMISSION_INTERFACE_EXTENSIONS_SLIDER_CRANK_TRANSMISSION_LOADER_HPP
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <ros/console.h>
+#include <transmission_interface/transmission_interface_exception.h>
+#include <transmission_interface/transmission_loader.h>
+#include <transmission_interface_extensions/common_namespaces.hpp>
+#include <transmission_interface_extensions/slider_crank_transmission.hpp>
+
+#include <boost/lexical_cast.hpp>
+
+namespace transmission_interface_extensions {
+
+class SliderCrankTransmissionLoader : public ti::TransmissionLoader {
+public:
+  virtual ti::TransmissionSharedPtr load(const ti::TransmissionInfo &src_info) {
+    try {
+      SliderCrankTransmissionInfo dst_info;
+      parseActuator(src_info, &dst_info);
+      parseJoints(src_info, &dst_info);
+      return ti::TransmissionSharedPtr(new SliderCrankTransmission(dst_info));
+    } catch (const std::exception &ex) {
+      ROS_ERROR_STREAM("Exception caught on loading SliderCrankTransmission named "
+                       << src_info.name_ << ": " << ex.what());
+      return ti::TransmissionSharedPtr();
+    }
+  }
+
+private:
+  //
+  // helper functions to parse xml
+  //
+
+  // raw values in a xml element
+
+  static double parseOptionalValue(const TiXmlElement &xml, const std::string &child_name,
+                                   const double default_value) {
+    const TiXmlElement *const child(xml.FirstChildElement(child_name));
+    return child ? boost::lexical_cast< double >(child->GetText()) : default_value;
+  }
+
+  static double parseRequiredValue(const TiXmlElement &xml, const std::string &child_name) {
+    const TiXmlElement *const child(xml.FirstChildElement(child_name));
+    if (!child) {
+      throw ti::TransmissionInterfaceException("Missing required element <" + child_name + ">");
+    }
+    return boost::lexical_cast< double >(child->GetText());
+  }
+
+  // <actuator> element in xml
+
+  static void parseActuator(const ti::TransmissionInfo &src_info,
+                            SliderCrankTransmissionInfo *dst_info) {
+    // make sure exactly 1 actuator is given
+    if (src_info.actuators_.size() != 1) {
+      throw ti::TransmissionInterfaceException(
+          "Invalid number of actuators: " +
+          boost::lexical_cast< std::string >(src_info.actuators_.size()));
+    }
+
+    // parse params from the <actuator> element
+    dst_info->actuator_reduction =
+        parseOptionalValue(src_info.actuators_[0].xml_element_, "mechanicalReduction", 1.);
+  }
+
+  // <joint> element in xml
+
+  static std::size_t findJointByRole(const std::vector< ti::JointInfo > &joints,
+                                     const std::string &role) {
+    for (std::size_t i = 0; i < joints.size(); ++i) {
+      if (joints[i].role_ == role) {
+        return i;
+      }
+    }
+    throw ti::TransmissionInterfaceException("No joint with the role of " + role);
+  }
+
+  static void parseJoints(const ti::TransmissionInfo &src_info,
+                          SliderCrankTransmissionInfo *dst_info) {
+    // makes sure exactly 3 joint are given
+    if (src_info.joints_.size() != 3) {
+      throw ti::TransmissionInterfaceException(
+          "Invalid number of joints: " +
+          boost::lexical_cast< std::string >(src_info.joints_.size()));
+    }
+
+    // parse params from the <joint> element with "crank" role
+    dst_info->crank_joint_id = findJointByRole(src_info.joints_, "crank");
+    dst_info->crank_offset =
+        parseOptionalValue(src_info.joints_[dst_info->crank_joint_id].xml_element_, "offset", 0.);
+    dst_info->crank_length =
+        parseRequiredValue(src_info.joints_[dst_info->crank_joint_id].xml_element_, "length");
+
+    // parse params from the <joint> element with "bar" role
+    dst_info->bar_joint_id = findJointByRole(src_info.joints_, "bar");
+    dst_info->bar_length =
+        parseRequiredValue(src_info.joints_[dst_info->bar_joint_id].xml_element_, "length");
+
+    // parse params from the <joint> element with "slider" role
+    dst_info->slider_joint_id = findJointByRole(src_info.joints_, "slider");
+    dst_info->slider_offset_x =
+        parseOptionalValue(src_info.joints_[dst_info->slider_joint_id].xml_element_, "offsetX", 0.);
+    dst_info->slider_offset_y =
+        parseOptionalValue(src_info.joints_[dst_info->slider_joint_id].xml_element_, "offsetY", 0.);
+  }
+};
+
+} // namespace transmission_interface_extensions
+
+#endif
